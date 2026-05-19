adeb39b4afdd2d20055821110db3246468bcb25e
saustin02 <seaustin02@outlook.com>
2026-05-19 18:57:41 -0400

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/gamereviews/gamereviewapi/GameReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamereviews.gamereviewapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameReviewApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
