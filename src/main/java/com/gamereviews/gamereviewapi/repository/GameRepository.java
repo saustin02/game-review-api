b5ae122ade279491bb2248a4f967bf4339ea2f38
saustin02 <seaustin02@outlook.com>
2026-05-23 21:45:48 -0400

    Add User, Game, and Review entities with repositories

--- src/main/java/com/gamereviews/gamereviewapi/repository/GameRepository.java
+++ src/main/java/com/gamereviews/gamereviewapi/repository/GameRepository.java
@@ -1,4 +1,12 @@
 package com.gamereviews.gamereviewapi.repository;
 
-public class GameRepository {
+import com.gamereviews.gamereviewapi.entity.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
+    Game findByTitle(String title);
+    //Game findByGenre(String genre);//
+    //Game findByReleaseYear(Long releaseYear);//
 }
--- src/main/java/com/gamereviews/gamereviewapi/repository/ReviewRepository.java
+++ src/main/java/com/gamereviews/gamereviewapi/repository/ReviewRepository.java
@@ -1,4 +1,13 @@
 package com.gamereviews.gamereviewapi.repository;
 
-public class ReviewRepository {
+import com.gamereviews.gamereviewapi.entity.Review;
+import com.gamereviews.gamereviewapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByUser(User user);
 }
+
