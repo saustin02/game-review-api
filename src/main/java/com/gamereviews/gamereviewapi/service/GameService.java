6fa37909e1c5312c6fa30c231811dfb6df375a8b
saustin02 <seaustin02@outlook.com>
2026-06-13 14:21:11 -0400

    Add GameService with validation

---
+++ src/main/java/com/gamereviews/gamereviewapi/service/GameService.java
@@ -0,0 +1,44 @@
+package com.gamereviews.gamereviewapi.service;
+
+import com.gamereviews.gamereviewapi.repository.GameRepository;
+import com.gamereviews.gamereviewapi.entity.Game;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public Game createGame(String title, Integer releaseYear, String genre) {
+
+        if (title == null || title.isEmpty()) {
+            throw new RuntimeException("title is empty!");
+        }
+        if (gameRepository.findByTitle(title) != null) {
+            throw new RuntimeException("title already exists!");
+        }
+        if (releaseYear == null || releaseYear < 0 || releaseYear > LocalDate.now().getYear()) {
+            throw new RuntimeException("invalid release year!");
+        }
+        if (genre == null || genre.isEmpty()) {
+            throw new RuntimeException("genre is empty!");
+        }
+
+        Game game = new Game();
+        game.setTitle(title);
+        game.setReleaseYear(releaseYear);
+        game.setGenre(genre);
+
+        return gameRepository.save(game);
+    }
+
+    public List<Game> getAllGames() {
+        return gameRepository.findAll();
+    }
+
+
+}
